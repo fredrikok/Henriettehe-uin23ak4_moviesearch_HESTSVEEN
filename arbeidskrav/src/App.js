f65dbb4931770670449e5b9abfa85b7f277716c9
Henriette <Henriettehestsveen@gmail.com>
2023-03-22 10:18:09 +0100

    Test

--- arbeidskrav/src/App.js
+++ arbeidskrav/src/App.js
@@ -2,8 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import './css/main.css';
 import Main from './components/Main';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [resultat, setResultat] = useState([])
+  const [filter, setFilter] = useState('James Bond')
+  
+  const getMovies = async() => {
+      const response = await fetch(`http://www.omdbapi.com/s=${filter}&type=movie&apikey=bed90b0b`)
+      const data = await response.json()
+      setMovies(data.Search)
+  }
+
+  useEffect(() => {
+      getMovies()
+    },[])
+
+  console.log(movies)
+
+
   return (
     <Main />
   );
