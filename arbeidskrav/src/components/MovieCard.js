41732a2b408681e3951be9c7297d0c368150d5c8
Henriette <Henriettehestsveen@gmail.com>
2023-03-22 09:44:44 +0100

    Oppdatert Moviecard

--- arbeidskrav/src/components/MovieCard.js
+++ arbeidskrav/src/components/MovieCard.js
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import Main from "./Main";
+
 export default function MovieCard ({title, img, category}){
+    
+    const [movies, setMovies] = useState([])
+    const [resultat, setResultat] = useState([])
+    const [filter, setFilter] = useState('James Bond')
+    
+    const getMovies = async() => {
+        const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=bed90b0b`)
+        const data = await response.json()
+        getMovies(data)
+        setResultat(movies?.filter(items => items?.source?.name === filter))
+    }
+
+    console.log(resultat.hits)
+    
+    
     return (
         <article classname="movie-card">
             <img src={img} alt={title} />
@@ -6,3 +24,4 @@ export default function MovieCard ({title, img, category}){
         </article>
     )
 }
+
