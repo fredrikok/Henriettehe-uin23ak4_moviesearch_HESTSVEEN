5a3c379fb0577c6d57676bde8cf1c3090b228a77
Henriette <Henriettehestsveen@gmail.com>
2023-03-22 09:23:24 +0100

    Lagd main komponent og exportert

---
+++ arbeidskrav/src/components/Main.js
@@ -0,0 +1,11 @@
+import MovieCard from './MovieCard';
+
+export default function Main(){
+    return (
+        <>
+        <section>
+            <h2>Filmsøk</h2>
+        </section>
+        </>
+    )
+}
--- arbeidskrav/src/components/MovieCard.js
+++ arbeidskrav/src/components/MovieCard.js
@@ -1,5 +1,8 @@
-export default function MovieCard{
+export default function MovieCard ({title, img, category}){
     return (
-        
+        <article classname="movie-card">
+            <img src={img} alt={title} />
+            <h3>{title}</h3>
+        </article>
     )
 }
