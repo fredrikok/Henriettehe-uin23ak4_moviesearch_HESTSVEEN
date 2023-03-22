a8f03b70b78d93adc5642436720200ba548e8131
Henriette <Henriettehestsveen@gmail.com>
2023-03-22 12:04:13 +0100

    Oppdatert Layout

--- arbeidskrav/src/components/Layout.js
+++ arbeidskrav/src/components/Layout.js
@@ -1,5 +1,20 @@
-export default function Layout() {
+import { Link, Outlet } from "react-router-dom";
+
+export default function Layout(){
     return (
-        
+        <div id="container">
+          <header>
+            <Link to="/">
+                <h1>Filmer</h1>
+            </Link>
+            
+          </header>
+          <main>
+            <Outlet />
+          </main>
+          <footer>
+    
+          </footer>
+        </div>
     )
 }
