5aab07198da5f6bb50ec162ff3cc789ae22fd974
Henriette <Henriettehestsveen@gmail.com>
2023-03-22 17:35:02 +0100

    Endringer

--- arbeidskrav/src/index.js
+++ arbeidskrav/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//Endre litt på indexen, trenger vel ikke strictmode? Dobbeltsjekk dette :) 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
