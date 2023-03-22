f2c5a3f3a10dc91da9de521d558a92dbd7a669ff
Henriette <Henriettehestsveen@gmail.com>
2023-03-22 11:53:32 +0100

    Oppdatert søkefunksjon

--- arbeidskrav/src/components/SearchResults.js
+++ arbeidskrav/src/components/SearchResults.js
@@ -1,5 +1,20 @@
-export default function SearchResults{
+import {MagnifyingGlassIcon} from '@heroicons/react/24/outline'
+
+export default function Search({setSearch, getMovies}){
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+    }
+
+    const handleSearch = (event) =>{
+        setSearch(event.target.value)
+        console.log(event.target.value)
+    }
+    
     return (
-        
+        <form onSubmit={handleSubmit}>
+            <input type="search" placeholder="Søk etter film her.." onChange={handleSearch} />
+            <button type="submit" onClick={getMovies}><MagnifyingGlassIcon/></button>
+        </form>
     )
 }
