013f63912faf8d8c831d8f605b2ff9c289b55b60
Henriette <Henriettehestsveen@gmail.com>
2023-03-24 17:39:35 +0100

    Test_fjernet det som ikke trengs

--- arbeidskrav/src/App.js
+++ arbeidskrav/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import './css/main.css';
-import Main from './components/Main';
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import Layout from './components/Layout';
 import MovieCards from './components/MovieCards';
 
