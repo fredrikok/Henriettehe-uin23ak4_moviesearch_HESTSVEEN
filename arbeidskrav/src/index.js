adc07824052e6152d8f819aa6e69fea5a6f6a7bc
Henriette <Henriettehestsveen@gmail.com>
2023-03-24 17:22:55 +0100

    Mer testing

--- arbeidskrav/src/index.js
+++ arbeidskrav/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//Endre litt på indexen, trenger vel ikke strictmode? Dobbeltsjekk dette :) 
+//Endre litt på indexen?? Er det nødvendig med strictmode? Dobbeltsjekk dette :) 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
