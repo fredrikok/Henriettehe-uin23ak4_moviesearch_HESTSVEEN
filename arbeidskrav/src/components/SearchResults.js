55e441548a79c9e2f38748b0c3b614c7c0ee9946
Henriette <Henriettehestsveen@gmail.com>
2023-03-27 12:29:04 +0200

    Postet filmer - og linket

--- arbeidskrav/src/components/SearchResults.js
+++ arbeidskrav/src/components/SearchResults.js
@@ -1,7 +1,13 @@
 import MovieCards from "./MovieCards";
 
 export default function SearchResults(){
+
+    
+    handleSubmit = {filter};
+
     return (
+        //form - som går {handleSubmit}
         <MovieCards />
+        //event.target
     )
 }
