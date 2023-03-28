c4e173bb4dafe4e3d45bf2b5295d7ec4730ca166
Henriette <Henriettehestsveen@gmail.com>
2023-03-28 13:50:08 +0200

    Oppdatert

--- arbeidskrav/src/components/MovieCards.js
+++ arbeidskrav/src/components/MovieCards.js
@@ -1,5 +1,27 @@
-export default function MovieCards{
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+export default function MovieCards(){
+    
+    const [resultat, setResultat] = useState([])
+    const [filter, setFilter] = useState('James-Bond')
+  
+    const getMovies = async() => {
+      const response = await fetch(`http://www.omdbapi.com/?s=${filter}&type=movie&apikey=bed90b0b`)
+      const resultat = await response.json()
+      setResultat(resultat.Search)
+      console.log(resultat.Search)
+  }
+  useEffect(() => {
+      getMovies()
+    },[])
+    
     return (
-        
+        <>
+        {resultat.length <= 10 ? resultat?.map((item, index) =>(
+                <MovieCard key={index} Id={item.imdbID} Poster={item.Poster} Title={item.Title} Plot={item.Plot} Actors={item.Actors} Released={item.Released} Year={item.Year} />
+            )) : resultat?.map((item, index) =>(
+                <MovieCard key={index} Id={item.imdbID} Poster={item.Poster} Title={item.Title} Plot={item.Plot} Actors={item.Actors} Released={item.Released} Year={item.Year} />
+            )) } 
+        </> 
     )
-}
+    }
